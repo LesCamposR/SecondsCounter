f1d6e3d43cd323c35a2a35afb90cbf68f3ee9f64
LesCamposR <lesthercamposramirez91@gmail.com>
2023-03-16 21:47:11 +0000

    OneMore

--- src/js/index.js
+++ src/js/index.js
@@ -16,5 +16,36 @@ let minutes = 0;
 let tensOfMinutes = 0;
 let hours = 0;
 let tensOfHours = 0;
+
+setInterval(()=>{
+
+    seconds = seconds +1
+    
+    if (seconds > 9){
+        seconds = 0;
+        tensOfSeconds++;
+    }
+    
+    if (tensOfSeconds>5){
+        tensOfSeconds = 0;
+        minutes++;
+    }
+    
+    if (minutes>9){
+        minutes = 0;
+        tensOfMinutes++;
+    }
+    
+    if (tensOfMinutes>5){
+        tensOfMinutes = 0;
+        hours++;
+    }
+    
+    if (hours>9){
+        hours = 0;
+        tensOfHours++;
+    }
 ReactDOM.render(<SecondsCounter secondsCount={seconds} tensOfSecondsCount={tensOfSeconds} minutesCount={minutes}
-tensOfMinutesCount={tensOfMinutes} hoursCount={hours} tensOfHours={tensOfHours}/>, document.querySelector("#app"));
+tensOfMinutesCount={tensOfMinutes} hoursCount={hours} 
+tensOfHours={tensOfHours}
+/>, document.querySelector("#app"));100})
